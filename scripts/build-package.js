f0221968818affcd72a03918b34c77c9f72efecb
Jacob Blakely <jacob@codekirei.com>
2016-05-04 17:14:01 -0700

    fix build-package

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -1,6 +1,6 @@
 'use strict'
 
 const write = require('fs').writeFile
-const package = require('../package')
+const packageJson = require('../package')
 
-write('package.json', JSON.stringify(package), err => { if (err) throw err })
+write('package.json', JSON.stringify(packageJson), err => { if (err) throw err })
